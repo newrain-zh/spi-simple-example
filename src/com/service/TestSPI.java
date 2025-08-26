e2a6336b1b44c6e16d2b75fd72e2daf563696a73
newrain-zh <496268609@qq.com>
2025-08-26 17:27:15 +0800

    删除冗余类

--- src/com/service/TestSPI.java
+++ src/com/service/TestSPI.java
@@ -3,12 +3,15 @@ package com.service;
 import com.example.spi.LoggerService;
 
 public class TestSPI {
-
+    private  static final LoggerService LOG = LoggerService.getService();
 
     public static void main(String[] args) {
         LoggerService loggerService = LoggerService.getService();
         loggerService.info("你好");
         loggerService.debug("测试Java SPI 机制");
+
+        LOG.info("你好");
+        LOG.debug("测试Java SPI 机制");
     }
 
 }
